16c5512be886a115bba476525f1ce02e25e64054
Cristopher Herrera <cristopher.herrera@epn.edu.ec>
2022-02-01 20:08:41 -0500

    feat: add two files in fundamentals directory
    Add Input & Output file
    Add Variables file
    Reviewing fundamental concepts
    
    cristoxdxd

---
+++ Fundamentals/02.InputOutput.py
@@ -0,0 +1,9 @@
+# Input Funtion & Output Funtion
+print('\tForm')
+# print('Name: ', end = '')
+names = input('Names: ')
+lastnames = input('Lastnames: ')
+age = input('Age: ')
+
+print('\n\tRegister Successful')
+print('Names:',names,'\nLastnames:',lastnames,'\nAge:',age)
---
+++ Fundamentals/03.Variables.py
@@ -0,0 +1,9 @@
+greet = 'Hi!'
+magicnumber = 5
+badgrade = 2.5
+validator = True
+print('\tPrimitive Variables')
+print('String: ',type(greet),
+    '\nInteger:',type(magicnumber),
+    '\nFloat:  ',type(badgrade),
+    '\nBoolean:',type(validator))
