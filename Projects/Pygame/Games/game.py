ad24022d6fe15a7d91bf2d3516e8aaee18061850
crstxkng <crstxkng@localhost.localdomain>
2022-03-28 20:43:36 -0500

    feat: add tkinker directory
    Fix some little errors
    Add the Tkinter directory

--- Projects/Pygame/Games/game.py
+++ Projects/Pygame/Games/game.py
@@ -1,4 +1,6 @@
+import math
 import pygame
+from pygame import mixer
 from Characters.Characters import Ghost, Monster
 from Characters.Position import Position
 
@@ -9,7 +11,24 @@ initialPosition = {
     'ghost': Position((100,10))
 }
 
+background = pygame.image.load('./assets/img/background.jpg')
+
 pygame.init()
+
+Points = 0
+font1 = pygame.font.Font('freesansbold.ttf',20)
+font2 = pygame.font.SysFont('Segoe UI',18)
+
+mixer.music.load('./assets/audio.wav')
+mixer.music.play(-1)
+
+def show_texts():
+    text1 = font1.render('Puntos: '+str(puntos), True, (255,255,255))
+    text2 = font1.render('Python Club de software EPN', True, (255,255,255))
+    text3 = font1.render('Cristopher Herrera', True, (255,255,255))
+    screen.blit(text1,(25,440))
+    screen.blit(text2,(280,430))
+    screen.blit(text3,(292,455))
 
 screen = pygame.display.set_mode(ScreenResolution)
 
@@ -25,6 +44,7 @@ end = False
 
 while not end:
     screen.fill(BackgroundColor)
+    screen.blit(background,(0,0))
 
     # Game settings
     pygame.display.set_caption("Mounsters")  
@@ -54,7 +74,7 @@ while not end:
             if event.key == pygame.K_DOWN:
                 moveDown = False              
 
-    # Efectuar movimientos
+    # Make moves
     if moveLeft:
         ghost.position.setX(ghost.position.getCoordinates()[0]-1)
     elif moveRight:
@@ -63,6 +83,26 @@ while not end:
         ghost.position.setY(ghost.position.getCoordinates()[1]-1)
     elif moveDown:
         ghost.position.setY(ghost.position.getCoordinates()[1]+1)
+
+    CharacterCoordinates = ghost.position.getCoordinates()
+
+    # Borders control
+    if CharacterCoordinates[0] < 0:
+        ghost.position.setX(0)
+    elif CharacterCoordinates[0] > 500:
+        ghost.posicion.setX(500)
+    if CharacterCoordinates[1] < 0:
+        ghost.posicion.setY(0)
+    elif CharacterCoordinates[1] > 320:
+        ghost.posicion.setY(320)
+
+    d = math.sqrt((monster.position.getCoordinates()[0] - ghost.position.getCoordinates()[0])**2+(monster.position.getCoordinates()[1] - ghost.position.getCoordinates()[1])**2)
+
+    print('Distancia: ',d)
+    if d < 30:
+        scream = mixer.Sound('./assets/grito.wav')
+        scream.play()
+        Points +=1
 
     ghost.draw()
     monster.draw()
